1168470693d9d2fe03e7bb4b518cfdd07b725ad6
Jan Hegewald <jan.hegewald@awi.de>
2018-08-14 11:45:24 +0200

    - add class to represent a Fesom output file - be able to tell the frequency for a given output file

--- lib/fesom_output_dir.rb
+++ lib/fesom_output_dir.rb
@@ -14,3 +14,33 @@ class FesomOutputDir
   end
   
 end
+
+
+class FesomOutputFile
+  # fetch frequency from native fesom file
+  # https://github.com/WCRP-CMIP/CMIP6_CVs/blob/master/CMIP6_frequency.json
+  def self.frequency(f)
+    sout = %x(ncdump -h #{f} | grep output_schedule)
+    /unit: (?<unit>\w) / =~ sout
+    /rate: (?<rate>\d+)/ =~ sout
+  
+    case "#{unit}#{rate}"
+    when "y1"
+      "yr"
+    when "m1"
+      "mon"
+    when "d1"
+      "day"
+    when /s\d+/
+      # this frequency is based on fesom time steps, i.e. might be used to express 3-hourly
+      puts "NOTE: assuming frequency rate of #{unit}#{rate} equals 3hr for #{f}"
+      # solution: read the time axis and see what delta t we really have
+
+      # for tso the frequency should be 3hrPt now (i.e. instantaneous)      
+      "3hrPt" # this is hackish but does apply for the PRIMAVERA runs (tso is the only 3-hourly variable)      
+    else
+      raise "unknown unit+rate <#{unit}#{rate}> for file <#{f}>"
+    end    
+  end
+
+end
