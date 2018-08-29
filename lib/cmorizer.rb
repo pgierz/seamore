1f2e529e1ad22a382c1202bb67fa535a0306d0f6
Jan Hegewald <jan.hegewald@awi.de>
2018-08-29 08:22:07 +0200

    - store the created Task instances in Project object - define execute method in Project to call execute in each Task - create Step instances in Task object - store the created Step instances in Task object - define execute method in Task to call execute in each Step

--- lib/cmorizer.rb
+++ lib/cmorizer.rb
@@ -3,7 +3,13 @@ require_relative "step.rb"
 module CMORizer
   class Project
     def initialize(src_txt)
+      @tasks = []
       instance_eval(src_txt, src_txt)
+    end
+  
+  
+    def execute
+      @tasks.each {|t| t.execute}
     end
   
   
@@ -12,7 +18,7 @@ module CMORizer
       src = hash.keys.first
       results = hash.values.first
       results.each do |r|
-        Task.new(src, r, &block)
+        @tasks << Task.new(src, r, &block)
       end
     end
 
@@ -25,22 +31,28 @@ module CMORizer
 
 
   class Task
-    def initialize(src, result, &block)
-      puts "#{self.class} #{src} ==> #{result}"
-      @src = src
-      @result = result
+    def initialize(from, to, &block)
+      @fesom_variable = from
+      @cmor_variable = to
+      @steps = []
       @eval_mode = true
       instance_eval(&block) if block_given?
       @eval_mode = false
+    end
+    
+    
+    def execute
+      puts "#{self.class} #{@fesom_variable} ==> #{@cmor_variable}"
+      @steps.each {|s| s.execute}
     end
 
 
     def method_missing(method_sym, *args, &block)
       return super unless @eval_mode
       # we assume every unknown method designates a sub-task
-      hash = args.first
-      from = hash.keys.first
-      to = hash.values.first
+      sym = method_sym.upcase
+      cls = CMORizer::Step.const_get sym
+      @steps << cls.new
     end
 
 
@@ -64,5 +76,6 @@ if __FILE__ == $PROGRAM_NAME
   end
   EOFHEREDOC
   
-  CMORizer::Project.new src_txt
+  project = CMORizer::Project.new src_txt
+  project.execute
 end
--- lib/step.rb
+++ lib/step.rb
@@ -1,9 +1,25 @@
 module CMORizer
   module Step
-    class Unit_K_to_degC
-      def initialize
+    class BaseStep
+      def execute
         puts "\t#{self.class}"
       end
+    end
+    
+    
+    class MERGEFILES < BaseStep
+    end
+    
+    
+    class FESOM_MEAN_TIMESTAMP_ADJUST < BaseStep
+    end
+    
+    
+    class Unit_K_to_degC < BaseStep
+    end
+    
+
+    class TIME_SECONDS_TO_DAYS < BaseStep
     end
   end
 end
