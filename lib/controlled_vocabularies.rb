4a911db494ffdefe45dded4ab32d6f9d0f98b0d4
Jan Hegewald <jan.hegewald@awi.de>
2018-08-30 10:55:28 +0200

    - change ControlledVocabularies to represent all json files in a single Hash - add UT to test ControlledVocabularies can read experiment_id and source_id from a given directory or single file

--- lib/controlled_vocabularies.rb
+++ lib/controlled_vocabularies.rb
@@ -3,9 +3,25 @@ require "json"
 
 # https://github.com/WCRP-CMIP/CMIP6_CVs
 # https://docs.google.com/document/d/1h0r8RZr_f3-8egBMMh7aqLwy3snpD6_MrDz1q8n5XUk/edit
-class ControlledVocabularies
-  def initialize(cmip6_cvs_dir)
-    eliglible_files = Dir["#{cmip6_cvs_dir}/CMIP6_*.json"]
-    p eliglible_files
+class ControlledVocabularies < Hash
+  def self.new_from_dir(cmip6_cvs_dir)
+    ControlledVocabularies.new Dir["#{cmip6_cvs_dir}/*.json"]
   end
+
+  
+  def initialize(json_files)  
+    json_files.each do |f|
+      h = ControlledVocabularies.hash_from_json_file f
+      merge! h
+    end    
+  end
+
+
+  def self.hash_from_json_file(path)
+    begin
+      JSON.parse(File.read(path), object_class: Hash)
+    rescue JSON::ParserError => e
+      raise "file #{path}: #{e.message}"
+    end
+  end  
 end
--- test/test_controlled_vocabularies.rb
+++ test/test_controlled_vocabularies.rb
@@ -13,7 +13,28 @@ class ControlledVocabulariesTests < Minitest::Test
   
   
   def test_can_create_controlled_vocabularies_instance
-    cv = ControlledVocabularies.new "#{__dir__}/fixtures/CV"
+    cv = ControlledVocabularies.new ["#{__dir__}/fixtures/CV/CMIP6_experiment_id.json"]
     assert_kind_of ControlledVocabularies, cv
+  end
+
+
+  def test_can_read_experiment_id_json
+    cv = ControlledVocabularies.new ["#{__dir__}/fixtures/CV/CMIP6_experiment_id.json"]
+    assert cv['experiment_id']
+  end
+
+
+  def test_can_read_start_year_from_experiment_id
+    cv = ControlledVocabularies.new ["#{__dir__}/fixtures/CV/CMIP6_experiment_id.json"]    
+    assert_equal "2015", cv['experiment_id']['highres-future']['start_year']
+  end
+
+
+  def test_can_read_experiment_id_and_source_id_from_directory
+    puts
+    cv = ControlledVocabularies.new_from_dir "#{__dir__}/fixtures/CV"
+    assert_equal "2015", cv['experiment_id']['highres-future']['start_year']
+    assert cv['experiment_id']
+    assert cv['source_id']
   end
 end
