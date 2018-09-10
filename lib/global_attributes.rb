275ed3b8257a2c6a9de8527bb55249d6746c65a1
Jan Hegewald <jan.hegewald@awi.de>
2018-09-10 14:26:36 +0200

    add class to represent the required global netcdf attributes

---
+++ lib/global_attributes.rb
@@ -0,0 +1,4 @@
+
+# required global netcdf attributes as described here: https://docs.google.com/document/d/1h0r8RZr_f3-8egBMMh7aqLwy3snpD6_MrDz1q8n5XUk/edit
+class GlobalAttributes
+end
---
+++ test/test_global_attributes.rb
@@ -0,0 +1,13 @@
+require_relative "../lib/global_attributes.rb"
+
+require "minitest/autorun"
+
+
+class GlobalAttributesTests < Minitest::Test
+  def setup
+  end
+  
+  
+  def teardown
+  end
+end
