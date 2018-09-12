4eb7a27c36e37546da3b70ef78d5005daf771e4f
Jan Hegewald <jan.hegewald@awi.de>
2018-09-12 15:26:43 +0200

    change indentation

--- test/test_global_attributes.rb
+++ test/test_global_attributes.rb
@@ -17,11 +17,9 @@ class GlobalAttributesTests < Minitest::Test
     builder.set_experiment_info(id: "highres-future", variant_label: "r1i1p1f002", first_year: 2000)
     builder.set_parent_experiment_info(id: "control", variant_label: "r1i1p1f002", first_year: 1900)
     builder.set_variable_info(id: "so", frequency: "mon", table_id: "Omon", table_realm: "ocean")
-    builder.set_grid_info(
-                        source_id: "AWI-CM-1-0-LR",
+    builder.set_grid_info(source_id: "AWI-CM-1-0-LR",
                         nominal_resolution: "50 km",
-                        txt: "FESOM 1.4 (unstructured grid in the horizontal with 126859 wet nodes; 46 levels; top grid cell 0-5 m)",
-                        )
+                        txt: "FESOM 1.4 (unstructured grid in the horizontal with 126859 wet nodes; 46 levels; top grid cell 0-5 m)")
     
     ga = builder.build_global_attributes(data_specs_version: "01.00.27")
     assert "so", ga.attributes['variable_id']
