d2910a42a5005c80d6d6278eb33d7a73d886abe9
Jan Hegewald <jan.hegewald@awi.de>
2019-09-06 13:17:30 +0200

    fix typo in internal variable name

--- lib/fesom_output_dir.rb
+++ lib/fesom_output_dir.rb
@@ -4,10 +4,10 @@ class FesomOutputDir
   attr_reader :variable_files
 
   def initialize(d, first_year=nil, last_year=nil)
-    eliglible_files = Dir[File.join(d,"*")].grep(/\/(?<variable_id>\w+)_fesom_\d{8}\.nc\Z/)
-
+    eligible_files = Dir[File.join(d,"*")].grep(/\/(?<variable_id>\w+)_fesom_\d{8}\.nc\Z/)
+    
     @variable_files = []
-    eliglible_files.each do |f|
+    eligible_files.each do |f|
       /(?<variable_id>\w+)_fesom_(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})\.nc\Z/ =~ File.basename(f)
       if(first_year && last_year) # might be nil, i.e. read every year
         if(first_year <= year.to_i && year.to_i <= last_year)
