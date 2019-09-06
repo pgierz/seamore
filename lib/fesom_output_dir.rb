191e4ff9012597a7368c9472c67607da4e815a49
Jan Hegewald <jan.hegewald@awi.de>
2019-09-06 13:30:03 +0200

    remove unused experimental code

--- lib/fesom_output_dir.rb
+++ lib/fesom_output_dir.rb
@@ -26,42 +26,4 @@ class FesomOutputDir
     @variable_files.inject('') {|sum,f| sum + "#{f}\n"}
   end
   
-  
-  #PRIMAVERA_DELIVERED_VARS = %w(evs fsitherm hfds mlotst omldamax opottemptend pbo prlq prsn rsdo siarean siareas sic sidmassevapsubl sidmasssi sidmassth sidmasstranx sidmasstrany siextentn siextents sifllatstop sisnconc sisnmass sisnthick sispeed sistrxdtop sistrxubot sistrydtop sistryubot sithick sitimefrac siu siv sivol sivoln sivols so soga sos tauuo tauvo thetao thetaoga tos tso u2o uo uso uto v2o vo volo vso vto w2o wfo wo wso wto zos zossq)
-  
-  def report_on_data_request(data_request_tables_dir)
-    request = DataRequest.new_from_tables_dir(data_request_tables_dir)
-    request_vars = request.variable_ids
-    
-    # see if we have each existing fesom variable in the data request
-    @variable_files.each do |fevar|
-#    PRIMAVERA_DELIVERED_VARS.each do |fevar|
-      cmorvar = FesomVariable.variable_id_for_data_request("550", "01.00.27", fevar)
-      if(request_vars.include? cmorvar)
-        if(cmorvar == fevar)
-          puts "FESOM #{fevar}"
-        else
-          puts "FESOM #{fevar} => CMOR #{cmorvar}"
-       end
-      else
-        puts "FESOM #{fevar} !! can not assign to any CMOR variable"
-      end
-    end
-  end
-    
-end
-
-
-class FesomVariable
-    def self.variable_id_for_data_request(fesom_svn_revision, request_version, fesom_variable)
-      mappings = {}
-      mappings["550"] = {}
-      mappings["550"]["01.00.27"] = {}
-      mappings["550"]["01.00.27"]["tso"] = "tos"
-      
-      v = mappings[fesom_svn_revision][request_version][fesom_variable]
-      v ||= fesom_variable
-      #puts "FESOM revision #{fesom_svn_revision}::#{fesom_variable} => data request #{request_version}::#{v}"
-      v
-    end
 end
