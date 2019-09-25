e331828f22d1ccaaf3128affc9c7928e6f986629
Jan Hegewald <jan.hegewald@awi.de>
2019-09-25 17:11:07 +0200

    change print_matching to output a cmorize instruction if a unit does not match but if it could be converted automatically

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/fesom_output_dir.rb"
 require_relative "../lib/data_request.rb"
 require_relative "../lib/fesom_possible_var.rb"
+require_relative "../lib/step.rb"
 
 
 class Matcher
@@ -23,17 +24,25 @@ class Matcher
   def self.print_matching(request, fesom_variables)
     fesom_variables.uniq! {|fevar| [fevar.variable_id, fevar.time_method, fevar.unit]}
     fesom_variables.each do |fevar|
-      cmipvar = request.variables.find {|v| v.variable_id == fevar.variable_id && v.time_method == fevar.time_method && v.unit == fevar.unit}
+      # match the variable_id and time_method
+      cmipvar = request.variables.find {|v| v.variable_id == fevar.variable_id && v.time_method == fevar.time_method}
+
       if(cmipvar)
         table_txts = cmipvar.table_ids.map {|t| "#{cmipvar.variable_id}_#{t}"}
-        puts "cmorize #{fevar.variable_id}_#{fevar.frequency} => [#{table_txts.join(', ')}]"
-      else
-        cmipvar = request.variables.find {|v| v.variable_id == fevar.variable_id}
-        if(cmipvar)
-          puts "# NO match: #{fevar.variable_id} '#{fevar.unit}' #{fevar.frequency} #{fevar.time_method} (!! #{cmipvar.variable_id} '#{cmipvar.unit}' exists in datarequest)"
+        
+        # match also unit
+        if(cmipvar.unit == fevar.unit)
+          puts "cmorize #{fevar.variable_id}_#{fevar.frequency} => [#{table_txts.join(', ')}]"
         else
-          puts "# NO match: #{fevar.variable_id} '#{fevar.unit}' #{fevar.frequency} #{fevar.time_method}"
+          # see if we could auto convert the unit
+          if CMORizer::Step::AUTO_CONVERT_UNIT.auto_convert_unit_possible?(fevar.unit, cmipvar.unit)
+            puts "cmorize #{fevar.variable_id}_#{fevar.frequency} => [#{table_txts.join(', ')}] # automatically converting unit from '#{fevar.unit}' to '#{cmipvar.unit}'"
+          else
+            puts "# NO match: #{fevar.variable_id} '#{fevar.unit}' #{fevar.frequency} #{fevar.time_method} (!! #{cmipvar.variable_id} '#{cmipvar.unit}' exists in datarequest)"
+          end
         end
+      else
+        puts "# NO match: #{  fevar.variable_id} '#{fevar.unit}' #{fevar.frequency} #{fevar.time_method}"
       end
     end
   end
