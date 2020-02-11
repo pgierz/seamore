dfc6b8ba2c23f20fadaa896d98387dcb04b42ea5
Jan Hegewald <jan.hegewald@awi.de>
2020-02-11 14:22:51 +0100

    bump version before releasing recent fixes

--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,3 @@
 module Seamore
-  VERSION = '2.1.0' # version number according to semantic versioning (SemVer) paradigm
+  VERSION = '2.1.1' # version number according to semantic versioning (SemVer) paradigm
 end
