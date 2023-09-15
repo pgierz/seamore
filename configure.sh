5045d99222204761b6ee445982c5544df1eb738f
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2023-09-15 18:00:29 +0200

    more fixes to the automatic configuration

--- configure.sh
+++ configure.sh
@@ -6,13 +6,24 @@ MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 cd ${MY_PATH}
 
 # Local installation
-BUNDLE_PATH=$(realpath ${BUNDLE_PATH:-"lib/ruby/gems"})
+BUNDLE_PATH=${BUNDLE_PATH:-"lib"}
 mkdir -p ${BUNDLE_PATH}
-echo "export GEM_PATH=\${GEM_PATH}:${BUNDLE_PATH}" > ./.gem_path
+BUNDLE_PATH=$(realpath ${BUNDLE_PATH})
 
 source env.sh
 
+RUBY_VERSION=$(ruby --version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+echo "export GEM_PATH=\${GEM_PATH}:${BUNDLE_PATH}/ruby/${RUBY_VERSION}" > ./.gem_path
+
 echo "Installing bundle"
+bundle config set --local path "lib"
 bundle install
-gem install --install-dir ${BUNDLE_PATH} gli
+gem install --install-dir ${BUNDLE_PATH}/ruby/${RUBY_VERSION} gli
+
+echo "Downloading submodules"
+git submodule update --init
+
+echo "Installing ncn"
+cd gitmodules/ncn
+source env.sh
 
